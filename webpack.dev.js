09425a425994f762fc54a53d3808e8f80865e479
Ahmed Alsaqaf <ahmadalsaggaf30@gmail.com>
2020-12-04 23:23:53 -0500

    webPack mode

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,8 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'source-map',
   entry: './src/client/index.js',
   module: {
     rules: [
---
+++ webpack.prod.js
@@ -0,0 +1,44 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/client/index.js',
+  module: {
+    rules: [
+      {
+        test: '/\.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.json5$/i,
+        loader: 'json5-loader',
+        type: 'javascript/auto',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          // eslint options (if necessary)
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+        template: "./src/client/views/index.html",
+        filename: "./index.html",
+    })
+]
+};
